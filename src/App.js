12f18b71d46c321d026029a46be81a42635647b2
Nada Osama <nadaosama478@gmail.com>
2023-01-31 14:43:19 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import logo from './logo.svg';
-import {
-  RandomPicker,
-  RandomPickerChoice,
-  RandomPickerControls,
-} from './RandomPicker.js';
-import './App.scss';
+// import './App.css';
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import RandomPicker from './RandomPicker';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const changeHandler = (event) => {
+    // Passing file data (event.target.files[0]) to parse using Papa.parse
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log(results.data);
+        setData(results.data);
+      },
+    });
+  };
+
+  const namesList = [
+    'Marcelo',
+    'Lizzette',
+    'Pauline',
+    'Fumiko',
+    'Tomasa',
+    'Bertha',
+    'Antoinette',
+    'Tianna',
+    'Ammie',
+    'Victorina',
+    'Marlon',
+    'Jules',
+    'Arletha',
+    'Ellyn',
+    'Karol',
+    'Corrin',
+    'Josephine',
+  ];
   return (
-    <div className='App'>
+    <div>
+      {/* File Uploader */}
+      <input
+        type='file'
+        name='file'
+        accept='.csv'
+        onChange={changeHandler}
+        style={{ display: 'block', margin: '10px auto' }}
+      />
       <RandomPicker items={namesList} />
-      <RandomPickerChoice />
-      <RandomPickerControls />
     </div>
   );
 }
